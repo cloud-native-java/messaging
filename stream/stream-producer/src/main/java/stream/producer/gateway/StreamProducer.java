099760ce84ac2c5ba74d03beb876d5f881c15072
Josh Long <josh@joshlong.com>
2017-05-22 01:09:34 +0200

    lrp messsaging data edge

--- stream/stream-producer/src/main/java/stream/producer/gateway/StreamProducer.java
+++ stream/stream-producer/src/main/java/stream/producer/gateway/StreamProducer.java
@@ -14,16 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import stream.producer.ProducerChannels;
 
-// <3>
-@MessagingGateway
-interface GreetingGateway {
-
- @Gateway(requestChannel = ProducerChannels.BROADCAST)
- void broadcastGreet(String msg);
-
- @Gateway(requestChannel = ProducerChannels.DIRECT)
- void directGreet(String msg);
-}
 
 @SpringBootApplication
 @EnableBinding(ProducerChannels.class)
@@ -35,6 +25,17 @@ public class StreamProducer {
  public static void main(String args[]) {
   SpringApplication.run(StreamProducer.class, args);
  }
+}
+
+// <3>
+@MessagingGateway
+interface GreetingGateway {
+
+ @Gateway(requestChannel = ProducerChannels.BROADCAST)
+ void broadcastGreet(String msg);
+
+ @Gateway(requestChannel = ProducerChannels.DIRECT)
+ void directGreet(String msg);
 }
 
 @RestController
